1baac74a5fb1684a1c586184a5de7be3a99f3bdd
Nam Pho <nampho@me.com>
2017-07-19 20:13:45 -0400

    adding unit counts for each product_module_code within all product_group_code

--- brands.R
+++ brands.R
@@ -11,16 +11,31 @@ products <- subset(products, products$upc_ver_uc == 1) # only use version 1
 drinks <- c(1508, 507, 2506, 1503, 1006, 1020)
 names(drinks) <- c("non-carbonated", "juice", "milk", "carbonated", "coffee", "tea")
 
+units.tbl <- data.frame()
+
 for (drink in drinks) {
     print(drink)
 
     product_group <- subset(products, product_group_code == drink)
     pmcodes <- names(table(product_group$product_module_code))
 
+    unitct <- list()
     for (pmcode in pmcodes) {
-    	brands <- names(table(subset(product_group, product_module_code == pmcode)$brand_descr))
-
+    	print(paste0("    ", pmcode)) # debug
+    	pmcode.sub <- subset(product_group, product_module_code == pmcode)
+	
+	unitct[[ pmcode ]] <- table(pmcode.sub$size1_units)
+	
 	# writes the output as product_group_code then product_module_code
+    	brands <- names(table(pmcode.sub$brand_descr))
     	write.csv(brands, file=paste0("~/src/purchase-analysis/data/brands-", drink, "-", pmcode, ".txt"), quote=F, row.names=F)
     }
+    
+    units <- names(table(products$size1_units))
+    units <- as.matrix( units[units != ""] )
+    for (ct in unitct) { units <- merge(units, as.matrix(ct), all.x=T, by.x=1, by.y="row.names") }
+    units[is.na(units)] <- 0
+    colnames(units) <- c("Units", pmcodes)
+
+    write.csv(units, file=paste0("~/src/purchase-analysis/data/units-", drink, ".txt"), quote=F, row.names=F)
 }
