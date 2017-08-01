fe1c2f01c919c6d189dea686cf33751e86167ed6
Nam Pho <nampho@me.com>
2017-08-01 17:36:30 -0400

    update file names for clarity

--- brands.R
+++ brands.R
@@ -8,7 +8,9 @@ library(data.table)
 products <- fread("~/nielsen_extracts/RMS/Master_Files/Latest/products.tsv", integer64="character")
 products <- subset(products, products$upc_ver_uc == 1) # only use version 1
 
-drinks <- c(1508, 507, 2506, 1503, 1006, 1020)
+exclude <- read.csv("~/src/purchase-analysis/excluded-20170720.csv", stringsAsFactors=F)
+
+drinks <- c(1508, 507, 2506, 1503, 1006, 1020) # aka product_group_code
 names(drinks) <- c("non-carbonated", "juice", "milk", "carbonated", "coffee", "tea")
 
 units.tbl <- data.frame()
@@ -19,6 +21,8 @@ for (drink in drinks) {
     product_group <- subset(products, product_group_code == drink)
     pmcodes <- names(table(product_group$product_module_code))
 
+    pmcodes <- pmcodes[!(pmcodes %in% subset(exclude, product_group_code == drink)$product_module_code)] # exclude 
+
     unitct <- list()
     for (pmcode in pmcodes) {
     	print(paste0("    ", pmcode)) # debug
@@ -28,7 +32,7 @@ for (drink in drinks) {
 	
 	# writes the output as product_group_code then product_module_code
     	brands <- names(table(pmcode.sub$brand_descr))
-    	write.csv(brands, file=paste0("~/src/purchase-analysis/data/brands-", drink, "-", pmcode, ".txt"), quote=F, row.names=F)
+    	write.csv(brands, file=paste0("~/src/purchase-analysis/data/brands/brands-", drink, "-", pmcode, ".txt"), quote=F, row.names=F)
     }
     
     units <- names(table(products$size1_units))
@@ -37,5 +41,5 @@ for (drink in drinks) {
     units[is.na(units)] <- 0
     colnames(units) <- c("Units", pmcodes)
 
-    write.csv(units, file=paste0("~/src/purchase-analysis/data/units-", drink, ".txt"), quote=F, row.names=F)
+    write.csv(units, file=paste0("~/src/purchase-analysis/data/units/units-", drink, ".txt"), quote=F, row.names=F)
 }
